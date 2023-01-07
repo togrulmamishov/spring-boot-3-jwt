dfa48cb8b806c859fe7468f5bb221c2ee00ea916
Togrul Mamishov <togrul511@gmail.com>
2023-01-07 18:54:49 +0400

    Initial commit

---
+++ src/main/java/az/mamishov/SpringSecurity3JwtApplication.java
@@ -0,0 +1,13 @@
+package az.mamishov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity3JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity3JwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/mamishov/SpringSecurity3JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package az.mamishov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity3JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
